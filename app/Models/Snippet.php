24e37c3a892e919303e9da7206a6aa9acbf06dd1
Mehedi Hassan Sunny <mehedisunny@tedfo.com>
2021-10-03 00:39:54 +0600

    Update README.md file

--- app/Models/Snippet.php
+++ app/Models/Snippet.php
@@ -10,4 +10,14 @@ class Snippet extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function setCodeAttribute($value)
+    {
+        $this->attributes['code'] = htmlentities($value, ENT_QUOTES);
+    }
+
+    public function getCodeAttribute($value)
+    {
+        return html_entity_decode($value, ENT_QUOTES);
+    }
 }
