111deba3bca00babd4601ae888ced639816ea30a
Mehedi Hassan Sunny <sunnymehedihassan@gmail.com>
2021-10-12 10:13:40 +0600

    Update header file

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,7 @@
 <header class="header sticky-top">
     <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
         <div class="container"><a class="navbar-brand" href="#">
-                <strong class="h6 mb-0 font-weight-bold text-uppercase">The Remote Company</strong></a>
+                <strong class="h6 mb-0 font-weight-bold text-uppercase">The Company</strong></a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 @if (request()->is('administrator/*'))
